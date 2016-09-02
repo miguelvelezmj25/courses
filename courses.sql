6ceff45b58d5bc21ef07610721c9b61684090692
Miguel Velez <miguelvelez@mijecu25.com>
2016-09-02 12:49:52 -0400

    Add Professors from CMU

--- courses.sql
+++ courses.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # https://github.com/sequelpro/sequelpro
 #
-# Host: 127.0.0.1 (MySQL 5.7.13)
+# Host: 127.0.0.1 (MySQL 5.7.14)
 # Database: courses
-# Generation Time: 2016-08-12 17:51:32 +0000
+# Generation Time: 2016-09-02 16:49:35 +0000
 # ************************************************************
 
 
@@ -48,6 +48,8 @@ LOCK TABLES `classes` WRITE;
 
 INSERT INTO `classes` (`id`, `name`, `description`, `enrollment_id`, `term`, `year`, `professor_id`, `grade`, `created_at`, `updated_at`)
 VALUES
+	('17-797','Software Engineering Seminar','The Software Research Seminar (SSSG) at CMU relies on active participation and a supportive community to help students broaden their understanding of software research and hone their presentation skills. This document briefly outlines our expectations for Software Engineering Ph.D. students with respect to SSSG. General Expectations: A. Attend both SSSG presentations each week. B. Participate by asking questions, contributing to the discussion, and giving constructive verbal and written feedback to presenters. Using an electronic device for purposes other than note-taking or feedback on the presentation is considered to be non-participation. C. Give 3 well-prepared SSSG talks per year (2 if giving an approved talk in an outside venue) The faculty recognizes that students may occasionally have conflicts with particular SSSG dates; in these cases the Reasonable Person Principle applies. There may occasionally be a systematic conflict, such as an overlapping course, in which case advance ','0b2b51f4-712a-11e6-9a00-94e7c62e904a','Fall',2016,'63812ef0-712a-11e6-9a00-94e7c62e904a','','2016-09-02 12:36:46','2016-09-02 12:36:46'),
+	('17-808','Software Engineering Research','This course prepares PhD students to begin research in software engineering. It introduces important ideas across the breadth of software engineering and the major research strategies of the field. Students will become familiar with the structure of the field; they will learn the seminal ideas and developments that led to current research questions; they will learn to critique research papers to evaluate their claims and evidence; and they will also become familiar with the current research themes at CMU.','0b2b51f4-712a-11e6-9a00-94e7c62e904a','Fall',2016,'dad3f294-712a-11e6-9a00-94e7c62e904a','','2016-09-02 12:34:02','2016-09-02 12:35:35'),
 	('CISC 131','Intro-Programming&Prob Solving','This course is designed for students with majors in the Department of Computer and Information Sciences and focuses on logical thinking, the design and implementation of algorithms in a procedural language, testing, correctness, and the use of common programming structures such as arrays. In addition, basic machine concepts are covered including hardware organization and representation of information in the machine. The typical student will be adept at using the computer but will have no prior programming experience. Engineering and science majors should take CISC 130. Please see your academic advisor to ensure you select the appropriate class. Lab included. NOTE: Students who receive credit for CISC 131 may not receive credit for CISC 130','15867696-0b05-11e6-bbb5-e920e415be78','Fall',2011,'0d492c82-0b0d-11e6-bbb5-e920e415be78','A','2016-04-25 13:23:39','2016-04-25 13:55:42'),
 	('CISC 210','Information Security','An introductory course in computer security. Topics include operating system security, cryptography, user authentication, application security, secure programming, web security and privacy issues, and ethical issues in the field of computer security. Emphasis is on understanding the technical aspects of how adversaries exploit systems and the techniques for defending against these attacks. Prerequisites: 1) MATH 128 or ENGR 230 or STAT 220(IDTH 220) (may be taken concurrently), and 2) a minimum grade of C- in CISC 130 or 131','15867696-0b05-11e6-bbb5-e920e415be78','Spring',2013,'abe820c8-0b0d-11e6-bbb5-e920e415be78','A','2016-04-25 13:44:24','2016-04-25 14:10:10'),
 	('CISC 230','Object Oriented Design & Prog','(Formerly QMCS 281) Programming and problem solving using an object-oriented approach. Builds on the procedural language foundation developed in CISC 130 or 131. Topics include: how procedural design differs from object-oriented design, algorithms, modeling, design requirements and representation, Uniform Modeling Language specification, implementation of object-oriented models, testing, and verification, and elementary design patterns. Lab included Prerequisites: A minimum grade of C- in CISC 130 or 131\n\n','15867696-0b05-11e6-bbb5-e920e415be78','Spring',2012,'0d492c82-0b0d-11e6-bbb5-e920e415be78','A','2016-04-25 13:23:39','2016-04-25 14:10:03'),
@@ -105,6 +107,7 @@ LOCK TABLES `enrollment` WRITE;
 
 INSERT INTO `enrollment` (`id`, `degree`, `alias`, `university_id`, `level`, `start`, `end`, `created_at`, `updated_at`)
 VALUES
+	('0b2b51f4-712a-11e6-9a00-94e7c62e904a','Doctor of Philosophy','Ph.D.','499d7fb4-60b5-11e6-b92d-83017b6792a5','graduate','2016-08-29',NULL,'2016-09-02 12:26:47','2016-09-02 12:26:47'),
 	('15867696-0b05-11e6-bbb5-e920e415be78','Bachelor of Arts','B.A.','d45e7e2c-0af3-11e6-bbb5-e920e415be78','undergraduate','2010-09-01','2015-12-18','2016-04-25 11:45:14','2016-04-25 11:52:23'),
 	('87cb9056-0b05-11e6-bbb5-e920e415be78','Certificate of Attendance',' ','ce6af6ee-0af3-11e6-bbb5-e920e415be78','undergraduate','2015-06-01','2015-08-01','2016-04-25 11:48:26','2016-04-25 14:07:13');
 
@@ -152,15 +155,26 @@ VALUES
 	('2d8e6cf2-0b15-11e6-bbb5-e920e415be78','Thanaa Ghanem','CISC','d45e7e2c-0af3-11e6-bbb5-e920e415be78','','','','2016-04-25 13:40:26','2016-04-25 13:40:26'),
 	('371882dc-0b0e-11e6-bbb5-e920e415be78','Misha Shvartsman','MATH','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5527','mmshvartsman@stthomas.edu','OSS 213','2016-04-25 12:50:36','2016-04-25 12:50:36'),
 	('40ded88a-0b0d-11e6-bbb5-e920e415be78','German Pliego','CISC','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5377','gjpliego@stthomas.edu','OSS 410','2016-04-25 12:43:43','2016-04-25 12:43:43'),
+	('63812ef0-712a-11e6-9a00-94e7c62e904a','Jonathan Aldrich','ISR','499d7fb4-60b5-11e6-b92d-83017b6792a5','412-268-7278','jonathan.aldrich@cs.cmu.edu ','WEH 4216','2016-09-02 12:29:15','2016-09-02 12:43:54'),
+	('7136706e-712a-11e6-9a00-94e7c62e904a','David Garlan','ISR','499d7fb4-60b5-11e6-b92d-83017b6792a5','412-268-5056','garlan@cs.cmu.edu','WEH 4218','2016-09-02 12:29:38','2016-09-02 12:44:03'),
 	('73e22240-0b0e-11e6-bbb5-e920e415be78','Adam Green','PHYS','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5289','asgreen@stthomas.edu','OWS 165C','2016-04-25 12:52:18','2016-04-25 12:52:18'),
 	('75527fa4-0b0d-11e6-bbb5-e920e415be78','Armando Solar-Lezama','EECS','ce6af6ee-0af3-11e6-bbb5-e920e415be78','617-258-9727','asolar@csail.mit.edu','32-G840','2016-04-25 12:45:11','2016-04-25 12:45:58'),
+	('7732864c-712a-11e6-9a00-94e7c62e904a','Mary Shaw','ISR','499d7fb4-60b5-11e6-b92d-83017b6792a5','412-268-2589','Mary.Shaw@cs.cmu.edu','WEH 5123','2016-09-02 12:29:48','2016-09-02 12:46:31'),
 	('89949078-0b0e-11e6-bbb5-e920e415be78','Marty Johnson','PHYS','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5214','mejohnston@stthomas.edu','OWS 160A','2016-04-25 12:52:54','2016-04-25 12:52:54'),
 	('8d64b63e-0b0d-11e6-bbb5-e920e415be78','Mark Werness','CISC','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5471','mewerness@stthomas.edu','OSS 403','2016-04-25 12:45:51','2016-04-25 12:45:51'),
+	('8e9486b4-712a-11e6-9a00-94e7c62e904a','Brad Myers','HCII','499d7fb4-60b5-11e6-b92d-83017b6792a5','412-268-5150','bam@cs.cmu.edu','NSH 3517','2016-09-02 12:30:27','2016-09-02 12:43:43'),
+	('973844cc-712a-11e6-9a00-94e7c62e904a','Travis Breaux','ISR','499d7fb4-60b5-11e6-b92d-83017b6792a5','412-268-7334','breaux@cs.cmu.edu','WEH 5103','2016-09-02 12:30:42','2016-09-02 12:44:29'),
+	('9f168f28-712a-11e6-9a00-94e7c62e904a','Jim Herbsleb','ISR','499d7fb4-60b5-11e6-b92d-83017b6792a5','412-268-8933','jdh@cs.cmu.edu','WEH 5307','2016-09-02 12:30:55','2016-09-02 12:44:56'),
 	('a3fa3148-0b0e-11e6-bbb5-e920e415be78','Marie Lopez del Puerto','PHYS','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5213','mlpuerto@stthomas.edu','OWS 160B','2016-04-25 12:53:38','2016-04-25 12:53:38'),
+	('a671f7b2-712a-11e6-9a00-94e7c62e904a','Bodgan Vasilescu','ISR','499d7fb4-60b5-11e6-b92d-83017b6792a5','','','','2016-09-02 12:31:07','2016-09-02 12:44:58'),
 	('abe820c8-0b0d-11e6-bbb5-e920e415be78','Scott Yilek','CISC','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5395','syilek@stthomas.edu','OSS 413','2016-04-25 12:46:42','2016-04-25 12:46:42'),
+	('b0ae8ce0-712a-11e6-9a00-94e7c62e904a','Christian Kastner','ISR','499d7fb4-60b5-11e6-b92d-83017b6792a5','412-268-5254','kaestner@cs.cmu.edu','WEH 5122','2016-09-02 12:31:24','2016-09-02 12:46:50'),
+	('b8bd7144-712a-11e6-9a00-94e7c62e904a','Bill Scherlis','ISR','499d7fb4-60b5-11e6-b92d-83017b6792a5','412-268-8741','wls@cs.cmu.edu','WEH 5222','2016-09-02 12:31:38','2016-09-02 12:47:20'),
 	('b98c700c-0b0e-11e6-bbb5-e920e415be78','Paul Ohmann','PHYS','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5224','prohmann@stthomas.edu','OWS 165B','2016-04-25 12:54:15','2016-04-25 12:54:18'),
 	('c7a12648-0b0d-11e6-bbb5-e920e415be78','Michael Hardy','MATH','d45e7e2c-0af3-11e6-bbb5-e920e415be78','','','','2016-04-25 12:47:29','2016-04-25 12:47:29'),
+	('d1ab3754-712a-11e6-9a00-94e7c62e904a','Dan Siewiorek','HCII','499d7fb4-60b5-11e6-b92d-83017b6792a5','412-268-2570','dps@cs.cmu.edu','NSH 4515','2016-09-02 12:32:20','2016-09-02 12:47:46'),
 	('d929405e-0b0c-11e6-bbb5-e920e415be78','Jason Sawin','CISC','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5478','jason.sawin@stthomas.edu','OSS 406 ','2016-04-25 12:39:30','2016-04-25 12:42:20'),
+	('dad3f294-712a-11e6-9a00-94e7c62e904a','Claire Le Goues','ISR','499d7fb4-60b5-11e6-b92d-83017b6792a5','412-268-6954','clegoues@cs.cmu.edu','WEH 5117','2016-09-02 12:32:35','2016-09-02 12:48:24'),
 	('dfde04e2-0b0d-11e6-bbb5-e920e415be78','Doug Dokken','MATH','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5523','dpdokken@stthomas.edu','OSS 201','2016-04-25 12:48:09','2016-04-25 12:48:09'),
 	('e74b08c8-0b0e-11e6-bbb5-e920e415be78','Richard Thomas','PHYS','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5212','rathomas1@stthomas.edu','OWS 162','2016-04-25 12:55:31','2016-04-25 12:55:31'),
 	('fdb677d8-0b0d-11e6-bbb5-e920e415be78','Melissa Shepard Loe','MATH','d45e7e2c-0af3-11e6-bbb5-e920e415be78','651-962-5534','msloe@stthomas.edu','OSS 212','2016-04-25 12:48:09','2016-04-25 12:48:09');
@@ -203,7 +217,7 @@ LOCK TABLES `universities` WRITE;
 
 INSERT INTO `universities` (`id`, `name`, `alias`, `address`, `city`, `state`, `zipcode`, `country`, `created_at`, `updated_at`)
 VALUES
-	('499d7fb4-60b5-11e6-b92d-83017b6792a5','Carnegie Mellon University','CMU','5000 Forbes Ave, Pittsburgh, PA 15213\n5000 Forbes Ave, Pittsburgh, PA 15213\n5000 Forbes Ave','Pittsburgh','PA','15213','USA','2016-08-12 13:50:42','2016-08-12 13:50:42'),
+	('499d7fb4-60b5-11e6-b92d-83017b6792a5','Carnegie Mellon University','CMU','5000 Forbes Ave','Pittsburgh','PA','15213','USA','2016-08-12 13:50:42','2016-09-02 12:23:51'),
 	('ce6af6ee-0af3-11e6-bbb5-e920e415be78','Massachusetts Institute of Technology','MIT','77 Massachusetts Ave','Cambridge','MA','02139','USA','2016-04-25 09:40:33','2016-07-14 20:58:31'),
 	('d45e7e2c-0af3-11e6-bbb5-e920e415be78','University of St Thomas','UST','2115 Summit Avenue','St Paul','MN','55105','USA','2016-04-25 10:18:10','2016-04-25 11:50:16');
 
